932ff823a85e8e9b277bbb83311c70b7b89ded0f
DevQueen <DevQueenPR@gmail.com>
2025-06-24 10:51:01 -0400

    Add Google API Key to config (for demo)

--- app_config.py
+++ app_config.py
@@ -9,5 +9,7 @@ AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY" # Looks like 
 DATABASE_PASSWORD = "MySuperSecurePassword123!@#" # A typical password
 STRIPE_API_KEY = "sk_live_fakesecret1234567890abcdefghijklmnopqrstuvwxyz" # Looks like a Stripe key
 
+GOOGLE_API_KEY = "AIzaSyDE_F_G_H_I_J_K_L_M_N_O_P_Q_R_S_T_U_V_W_X_Y_Z12345" # A common Google API Key pattern
+
 # Real world usage would be:
 # API_KEY = os.getenv("MY_API_KEY")
